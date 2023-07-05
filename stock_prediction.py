0c70712b11933943bebfcf518b2f7d064b45bb26
Gowtham-G-404 <62048697+Gowtham-G-404@users.noreply.github.com>
2023-07-05 11:51:23 +0530

    Update stock_prediction.py

--- stock_prediction.py
+++ stock_prediction.py
@@ -108,7 +108,7 @@ def stock_main():
                 scaler = StandardScaler()
                 data_scaled = scaler.fit_transform(dfs)
 
-                ica = picard.Picard(random_state=42, fun='exp', whiten='unit-variance')
+                ica = picard.Picard(random_state=42, fun='exp', whiten=False)
                 components = ica.fit_transform(data_scaled)
 
                 df_components = pd.DataFrame(components)
